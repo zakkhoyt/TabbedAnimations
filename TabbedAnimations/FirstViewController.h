fe6dbbdacc473f9a7baf98f18a65ce5fecd8d3cb
Zakk Hoyt <vaporwarewolf@gmail.com>
2015-05-14 21:24:45 -0700

    Initial Commit

---
+++ TabbedAnimations/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  TabbedAnimations
+//
+//  Created by Zakk Hoyt on 5/14/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ TabbedAnimations/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  TabbedAnimations
+//
+//  Created by Zakk Hoyt on 5/14/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
